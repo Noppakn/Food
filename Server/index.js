6d8bdb1b53b16233157a54ca830d5e18a1efd731
SakchaiSaehoei <sakchai.saeh@bumail.net>
2022-04-29 00:53:04 +0700

    implement login register change password

--- Server/index.js
+++ Server/index.js
@@ -56,6 +56,76 @@ app.put("/menu/:id", async (req,res) => {
         console.error(error.message)
     }
 })
+//login 
+app.get("/login", async (req,res) => {
+    try {
+        const data = req.body;
+        const login_info = await pool.query(
+            "SELECT * FROM user_info WHERE (username = $1) and (password = $2)",[data.username, data.password]
+        )
+        if (login_info.rows.length === 1){
+        res.json("Login Success")
+        }
+        else
+        {
+            res.json("Login failed")
+            }
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+//Register 
+
+app.post("/login", async (req,res) => {
+    try {
+        const data = req.body;
+        const exists = await pool.query(
+            "SELECT username FROM user_info WHERE (username = $1) and (password = $2)",[data.username, data.password]
+        )
+        if (exists.rows.length === 1){
+        res.json("this username is already exists")
+        }
+        else
+        {
+            const insert = await pool.query(
+                            "INSERT INTO user_info (username , password) VALUES ($1, $2)",[data.username, data.password]
+                        )
+                        if (insert){
+                        res.json("Registeration Success")
+                        }
+                        else
+                        {
+                            res.json("Registeration  failed")
+                            }
+            }
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+//Change password
+
+app.put("/login", async (req,res) => {
+    try {
+        const  body  = req.body;
+        const old_password = await pool.query(
+            "SELECT password FROM user_info WHERE (username = $1) and (password = $2)",[body.username, body.password]
+        )
+        if (old_password.rows.length === 1){
+         const update = await pool.query(
+           "UPDATE user_info SET password = $1 WHERE  username = $2",[body.newpasswrod,body.username]
+        )
+        res.json(" Password has been Updated!")
+        }
+        else{
+            res.json("user or password doesn't match")
+        }
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
 // status_order
 app.put("/update-admin-order", async (req,res) => {
     try {
